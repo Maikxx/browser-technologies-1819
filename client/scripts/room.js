0ac5e04accbcb393fb1482d5173559119c9329ec
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-23 21:30:47 +0100

    feat(room): add countdown timer

--- client/scripts/room.js
+++ client/scripts/room.js
@@ -1,4 +1,19 @@
 if (canMakeUseOfJavaScript()) {
-    var joinUrl = window.location.href + '/join'
+    var href = window.location.href
+    var joinUrl = href + '/join'
     new QRCode(document.getElementById('qrcode'), joinUrl)
+
+    var timeToWaitInSeconds = 90 * 1000
+    var countDownDate = new Date().getTime() + timeToWaitInSeconds
+    var interval = setInterval(function() {
+        var now = new Date().getTime()
+        var duration = countDownDate - now
+
+        setTextContentOfElement(document.getElementById('countdown-timer'), Math.round(duration / 1000))
+
+        if (duration < 0) {
+            clearInterval(interval)
+            window.location.replace(href + '/score')
+        }
+    }, 1000)
 }
