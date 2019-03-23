48ac86cd44a147879d83697532cac3bafc879430
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-23 17:48:09 +0100

    refactor(*): remove gulp-autoprefixer - no more prefixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,9 @@
 const gulp = require('gulp')
 const cleanCSS = require('gulp-clean-css')
-const autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('minifyCss', () => {
     return gulp.src('./client/css/*.css')
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({
-            browsers: [
-                'last 2 versions',
-                '> 1%',
-                'maintained node versions',
-                'not dead'
-            ],
-        }))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./server/public/css'))
 })
 
