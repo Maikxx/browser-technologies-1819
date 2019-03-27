be22e91882e84d9b7fbcfc771476ab1d2df216e1
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-27 15:00:32 +0100

    feat(featureDetection): fix featureDetection

--- server/public/scripts/featureDetection.js
+++ server/public/scripts/featureDetection.js
@@ -1,80 +1,17 @@
-function lastIndexOfExists() {
-    return 'lastIndexOf' in String.prototype
-        && typeof String.prototype.lastIndexOf === 'function'
-}
-
-function sliceExists() {
-    return 'slice' in String.prototype
-        && typeof String.prototype.slice === 'function'
-}
-
-function setAttributeExists() {
-    return 'setAttribute' in document.body
-        && typeof document.body.setAttribute === 'function'
-}
-
-function hasAttributeExists() {
-    return 'hasAttribute' in document.body
-        && typeof document.body.hasAttribute === 'function'
-}
-
-function classNameExists() {
-    return 'className' in document.body
-}
-
-function querySelectorAllExists() {
-    return 'querySelectorAll' in document
-        && typeof document.body.querySelectorAll === 'function'
-}
-
-function getElementsByClassNameExists() {
-    return 'getElementsByClassName' in document
-        && typeof document.getElementsByClassName === 'function'
-}
-
-function appendChildExists() {
-    return 'appendChild' in document.body
-        && typeof document.body.appendChild === 'function'
-}
-
-function getElementsByTagNameExists() {
-    return 'getElementsByTagName' in document.body
-        && typeof document.body.getElementsByTagName === 'function'
-}
-
-function getAttributeExists() {
-    return 'getAttribute' in document.body
-        && typeof document.body.getAttribute === 'function'
-}
-
-function locationPathExists() {
-    return 'pathname' in window.location
-}
-
-function getElementsByClassNameAlternative(className) {
-    var matches = []
-    var tags = document.getElementsByTagName("*")
-
-    for (var i = 0; i < tags.length; i++) {
-        var classNames = tags[i].className.split(' ')
-
-        for (var j = 0; j < classNames.length; j++) {
-            if (classNames[j] == className) {
-                matches.push(tags[i])
-            }
-        }
-    }
-
-    return matches
+function checkIfFeatureExists(feature, where, type) {
+    return feature in where
+        && type ?
+            typeof where[feature] === type
+            : true
 }
 
 function getHtmlElementsByClass(className) {
-    if (querySelectorAllExists()) {
+    if (checkIfFeatureExists('querySelectorAll', document.body, 'function')) {
         return document.querySelectorAll('.' + className)
-    } else if (getElementsByClassNameExists()) {
+    } else if (checkIfFeatureExists('getElementsByClassName', document.body, 'function')) {
         return document.getElementsByClassName(className)
     } else {
-        return getElementsByClassNameAlternative(className)
+        return getElementsByClassNamePolyfill(className)
     }
 }
 
@@ -98,12 +35,30 @@ function attachEventToElement(element, event, callback) {
     }
 }
 
+function getElementsByClassNamePolyfill(className) {
+    var matches = []
+    var tags = document.getElementsByTagName("*")
+
+    for (var i = 0; i < tags.length; i++) {
+        var classNames = tags[i].className.split(' ')
+
+        for (var j = 0; j < classNames.length; j++) {
+            if (classNames[j] == className) {
+                matches.push(tags[i])
+            }
+        }
+    }
+
+    return matches
+}
+
 function canMakeUseOfJavaScript() {
-    return lastIndexOfExists()
-        && getElementsByTagNameExists()
-        && getAttributeExists()
-        && hasAttributeExists()
-        && locationPathExists()
-        && sliceExists()
-        && classNameExists()
+    return checkIfFeatureExists('lastIndexOf', String.prototype, 'function')
+        && checkIfFeatureExists('slice', String.prototype, 'function')
+        && checkIfFeatureExists('getElementsByTagName', document.body, 'function')
+        && checkIfFeatureExists('setAttribute', document.body, 'function')
+        && checkIfFeatureExists('hasAttribute', document.body, 'function')
+        && checkIfFeatureExists('getAttribute', document.body, 'function')
+        && checkIfFeatureExists('pathname', window.location, 'function')
+        && checkIfFeatureExists('className', document.body, 'function')
 }
