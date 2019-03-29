efb1b52e6bacdd4c39661c6699cfe92016d6220e
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-29 12:47:39 +0100

    feat(index): improve checks

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -3,36 +3,41 @@ if (canMakeUseOfJavaScript()) {
     var addMoreOptionsForm = getHtmlElementsByClass('IncrementOptionsForm')[0]
     var questionFormFieldset = getHtmlElementsByClass('QuestionForm__fieldset')[1]
     var questionFormInputs = getHtmlElementsByClass('QuestionForm__input')
-    var lastQuestionFormInput = questionFormInputs[questionFormInputs.length - 1]
-    var lastQuestionFormInputNumber = Number(lastQuestionFormInput.id.slice(lastQuestionFormInput.id.indexOf('-') + 1))
-    var nextQuestionFormInputNumber = Number(lastQuestionFormInputNumber + 1)
+    if (questionFormInputs && questionFormInputs.length > 0) {
+        var lastQuestionFormInput = questionFormInputs[questionFormInputs.length - 1]
 
-    if (addMoreOptionsForm) {
-        attachEventToElement(addMoreOptionsForm, 'submit', function (event) {
-            if (event.preventDefault) {
-                event.preventDefault()
+        if (lastQuestionFormInput && lastQuestionFormInput.id) {
+            var lastQuestionFormInputNumber = Number(lastQuestionFormInput.id.slice(lastQuestionFormInput.id.indexOf('-') + 1))
+            var nextQuestionFormInputNumber = Number((lastQuestionFormInputNumber || 0) + 1)
+
+            if (addMoreOptionsForm) {
+                attachEventToElement(addMoreOptionsForm, 'submit', function (event) {
+                    if (event.preventDefault) {
+                        event.preventDefault()
+                    }
+
+                    if (questionFormFieldset && lastQuestionFormInput) {
+                        var labelElement = document.createElement('label')
+                        labelElement.htmlFor = 'option-' + nextQuestionFormInputNumber
+                        labelElement.className = 'QuestionForm__label'
+                        setTextContentOfElement(labelElement, 'Option ' + nextQuestionFormInputNumber)
+
+                        var inputElement = document.createElement('input')
+                        inputElement.type = 'text'
+                        inputElement.name = 'option-' + nextQuestionFormInputNumber
+                        inputElement.id = 'option-' + nextQuestionFormInputNumber
+                        inputElement.placeholder = 'Your option goes here'
+                        inputElement.className = 'QuestionForm__input Input'
+
+                        questionFormFieldset.appendChild(labelElement)
+                        questionFormFieldset.appendChild(inputElement)
+                        nextQuestionFormInputNumber++
+                    }
+
+                    return false
+                })
             }
-
-            if (questionFormFieldset && lastQuestionFormInput) {
-                var labelElement = document.createElement('label')
-                labelElement.htmlFor = 'option-' + nextQuestionFormInputNumber
-                labelElement.className = 'QuestionForm__label'
-                setTextContentOfElement(labelElement, 'Option ' + nextQuestionFormInputNumber)
-
-                var inputElement = document.createElement('input')
-                inputElement.type = 'text'
-                inputElement.name = 'option-' + nextQuestionFormInputNumber
-                inputElement.id = 'option-' + nextQuestionFormInputNumber
-                inputElement.placeholder = 'Your option goes here'
-                inputElement.className = 'QuestionForm__input Input'
-
-                questionFormFieldset.appendChild(labelElement)
-                questionFormFieldset.appendChild(inputElement)
-                nextQuestionFormInputNumber++
-            }
-
-            return false
-        })
+        }
     }
 }
 
